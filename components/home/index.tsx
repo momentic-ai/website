ccf846de2c14ebfe0b08d4bf54c2e38569b0d1e0
wuweiweiwu <hello@weiweiwu.me>
2023-10-29 16:33:48 -0700

    video

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,11 +1,13 @@
 import { CTA } from "./cta";
 import { FAQ } from "./faq";
 import { Hero } from "./hero";
+import { Video } from "./video";
 
 export function Home() {
   return (
     <main className="space-y-40 mb-40">
       <Hero />
+      <Video />
       <FAQ />
       <CTA />
     </main>
