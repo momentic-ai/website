e712334d1d33e43012eb8ac4e84d4d2f58ec5c69
wuweiweiwu <wei-wei@momentic.ai>
2023-12-09 21:37:33 -0800

    book a demo

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -59,7 +59,7 @@ export function Hero() {
                 className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-blue-600/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
               >
                 <span className="relative text-base font-semibold text-[#2971c7]">
-                  Book a call
+                  Book a demo
                 </span>
               </Link>
             </div>
