1fdfee54bf76c1a1f2c29e1bcec7f0ae33427946
wuweiweiwu <wei-wei@momentic.ai>
2024-01-10 15:37:05 -0800

    save work on running

--- components/frame.tsx
+++ components/frame.tsx
@@ -18,14 +18,14 @@ export const Frame = ({
       "my-4",
       border &&
         "p-1 pb-0 bg-gradient-to-tr from-blue-300/50 via-green-200/50 to-yellow-300/50 inline-block rounded-md sm:rounded-xl",
-      className
+      className,
     )}
   >
     <div
       className={cn(
         "inline-block rounded-md sm:rounded-xl overflow-hidden bg-primary/5 max-w-2xl [&>*]:mt-0 [&>*]:-mb-1",
         fullWidth && "max-w-full",
-        transparent && "bg-transparent"
+        transparent && "bg-transparent",
       )}
     >
       {children}
