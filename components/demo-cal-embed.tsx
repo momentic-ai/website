5607531eee402401a21042513b9b6affcadfa470
wuweiweiwu <wei-wei@momentic.ai>
2024-02-21 08:43:11 -0800

    update enterprise link

--- components/demo-cal-embed.tsx
+++ components/demo-cal-embed.tsx
@@ -16,7 +16,7 @@ export default function DemoCalEmbed() {
 
   return (
     <Cal
-      calLink="wuweiweiwu/momentic-demo"
+      calLink="wuweiweiwu/discovery"
       style={{ width: "100%", height: "100%", overflow: "scroll" }}
       config={{ layout: "month_view", theme: "light" }}
     />
